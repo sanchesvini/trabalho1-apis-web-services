3138ec95f73127f4f1b3ff82d651eb57c77ce0f8
sanchesvini <viniciuus.sanches.ramos@gmail.com>
2025-09-24 19:40:37 -0300

    chore: inicialização projeto API REST

---
+++ src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/Trabalho1apisApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.trabalho1apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho1apisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trabalho1apisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/utfpr/td/tsi/trabalho1apis/Trabalho1apisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.trabalho1apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho1apisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
