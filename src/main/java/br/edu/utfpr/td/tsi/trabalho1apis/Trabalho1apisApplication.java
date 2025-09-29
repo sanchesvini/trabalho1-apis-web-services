0a406cdf86740fc1fa9e62b0da784ffffca90c88
ViniciusLBernine <viniciusloucaobernine@gmail.com>
2025-09-29 06:05:36 -0300

    fix: habilitar repositórios JPA na aplicação

--- src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/Trabalho1apisApplication.java
+++ src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/Trabalho1apisApplication.java
@@ -2,8 +2,10 @@ package br.edu.utfpr.td.tsi.trabalho1apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("br.edu.utfpr.td.tsi.trabalho1apis.repository")
 public class Trabalho1apisApplication {
 
     public static void main(String[] args) {
