e4456576d49e1ef2039f7151e009cc747f96f83d
sanchesvini <viniciuus.sanches.ramos@gmail.com>
2025-10-11 00:24:10 -0300

    feat: implementa a classe CamposInvalidosException

---
+++ src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/exceptions/CamposInvalidosException.java
@@ -0,0 +1,8 @@
+package br.edu.utfpr.td.tsi.trabalho1apis.exceptions;
+
+public class CamposInvalidosException extends Exception {
+
+    public CamposInvalidosException(String message) {
+        super(message);
+    }
+}
