cc63e081f756c2a9a80f7253b0dd07abc3cd3d15
ViniciusLBernine <viniciusloucaobernine@gmail.com>
2025-10-12 05:27:07 -0300

    feat: centralização de metodos de busca

--- src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/repository/BoletimFurtoVeiculoRepository.java
+++ src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/repository/BoletimFurtoVeiculoRepository.java
@@ -2,14 +2,23 @@ package br.edu.utfpr.td.tsi.trabalho1apis.repository;
 
 import br.edu.utfpr.td.tsi.trabalho1apis.model.BoletimFurtoVeiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BoletimFurtoVeiculoRepository extends JpaRepository<BoletimFurtoVeiculo, Long> {
 
-    List<BoletimFurtoVeiculo> findByLocalOcorrenciaCidadeAndPeriodoOcorrencia(String cidade, String periodo);
+    List<BoletimFurtoVeiculo> findByLocalOcorrenciaCidadeAndPeriodoOcorrenciaContainingIgnoreCase(String cidade, String periodo);
 
-    List<BoletimFurtoVeiculo> findByLocalOcorrenciaCidade(String cidade);
+    List<BoletimFurtoVeiculo> findByLocalOcorrenciaCidadeContainingIgnoreCase(String cidade);
 
-    List<BoletimFurtoVeiculo> findByPeriodoOcorrencia(String periodo);
+    List<BoletimFurtoVeiculo> findByPeriodoOcorrenciaContainingIgnoreCase(String periodo);
+
+    List<BoletimFurtoVeiculo> findByVeiculoFurtadoEmplacamentoPlacaContainingIgnoreCase(String placa);
+
+    List<BoletimFurtoVeiculo> findByVeiculoFurtadoCorContainingIgnoreCase(String cor);
+
+    List<BoletimFurtoVeiculo> findByVeiculoFurtadoTipoVeiculoContainingIgnoreCase(String tipoVeiculo);
+
 }
