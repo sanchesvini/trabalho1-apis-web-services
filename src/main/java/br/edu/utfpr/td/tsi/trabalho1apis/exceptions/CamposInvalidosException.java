614cac58c4589ab63a66c92dcdfb0fcb492792bf
sanchesvini <viniciuus.sanches.ramos@gmail.com>
2025-10-13 15:10:42 -0300

    feat: remove classes não usadas

--- src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/exceptions/CamposInvalidosException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.utfpr.td.tsi.trabalho1apis.exceptions;
-
-public class CamposInvalidosException extends Exception {
-
-    public CamposInvalidosException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/edu/utfpr/td/tsi/trabalho1apis/repository/VeiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.utfpr.td.tsi.trabalho1apis.repository;
-
-import br.edu.utfpr.td.tsi.trabalho1apis.model.Veiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
-}
